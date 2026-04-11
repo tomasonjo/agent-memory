bd1e53c704ea4d14f761ae9ab733be961b0c23e4
William Lyon <lyonwj@gmail.com>
2026-04-11 12:02:28 -0600

    fixes

--- examples/financial-services-advisor/google-cloud-financial-advisor/backend/src/agents/__init__.py
+++ examples/financial-services-advisor/google-cloud-financial-advisor/backend/src/agents/__init__.py
@@ -3,22 +3,6 @@
 import inspect
 from functools import wraps
 
-from .aml_agent import create_aml_agent
-from .compliance_agent import create_compliance_agent
-from .kyc_agent import create_kyc_agent
-from .relationship_agent import create_relationship_agent
-from .supervisor import create_supervisor_agent, get_supervisor_agent
-
-__all__ = [
-    "bind_tool",
-    "create_supervisor_agent",
-    "get_supervisor_agent",
-    "create_kyc_agent",
-    "create_aml_agent",
-    "create_relationship_agent",
-    "create_compliance_agent",
-]
-
 
 def bind_tool(func, neo4j_service):
     """Create a wrapper that binds neo4j_service to a tool function.
@@ -37,3 +21,20 @@ def bind_tool(func, neo4j_service):
 
     wrapper.__signature__ = sig.replace(parameters=new_params)
     return wrapper
+
+
+from .aml_agent import create_aml_agent
+from .compliance_agent import create_compliance_agent
+from .kyc_agent import create_kyc_agent
+from .relationship_agent import create_relationship_agent
+from .supervisor import create_supervisor_agent, get_supervisor_agent
+
+__all__ = [
+    "bind_tool",
+    "create_supervisor_agent",
+    "get_supervisor_agent",
+    "create_kyc_agent",
+    "create_aml_agent",
+    "create_relationship_agent",
+    "create_compliance_agent",
+]
